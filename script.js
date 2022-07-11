b3eb1462eb13714c1bce00fccbf2c40963d1df8b
Mary Paul <marymc135@hotmail.com>
2022-07-11 10:39:21 -0400

    styling current time

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //Add current date to the top of the page using momentjs
-//make the task columsn editable
+//function getDate() {
+  //  var cDate=new Date();
+   // document.getElementbyId("Date")= cDate.toString();
+  //  console.log("date")
+
+const m = moment()
+console.log(m.format("MMMM Do YYYY"));
+//make event column editable
+//activate Save button
+//store data in local storage
+//color code columns based on past, present, future time
+//scroll down feature
+
