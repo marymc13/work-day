77d849c2373a1db079cc36f2ef3673971fb6da45
Mary Paul <marymc135@hotmail.com>
2022-07-12 00:23:22 -0400

    current date and time

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //Add current date to the top of the page using momentjs
-//function getDate() {
-  //  var cDate=new Date();
-   // document.getElementbyId("Date")= cDate.toString();
-  //  console.log("date")
-
+const date = document.getElementById("date")
 const m = moment()
-console.log(m.format("MMMM Do YYYY"));
-//make event column editable
+console.log(m.format("MMMM Do YYYY, hh:mm:ss A"));
+const NowMoment = moment(); 
+const eDate = document.getElementById('date');
+ eDate.innerHTML = NowMoment.format("MMMM Do YYYY, hh:mm:ss A");
+
+ //make event column editable
+ $(".list-group").on("click", "col-6", function() {
+  console.log(this);
+ })
 //activate Save button
 //store data in local storage
 //color code columns based on past, present, future time
